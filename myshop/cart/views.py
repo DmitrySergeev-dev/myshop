a966c73b39d1c2297f059f6e97ab789d849b359b
Dmitry Sergeev <dimonsergeev10@mail.ru>
2023-04-20 18:54:23 +0300

    Add views to app Cart and included it to project's urls.

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from cart import Cart
+from myshop.shop.models import Product
+from .forms import CartAddProductForm
+
+
+@require_POST
+def cart_add(request, product_id):
+    """Это представление добавления товаров в корзину или обновления количества существующих товаров"""
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        data = form.cleaned_data
+        cart.add(product=product,
+                 quantity=data['quantity'],
+                 override_quantity=data['override'])
+    return redirect('cart:cart_detail')
+
+
+@require_POST
+def cart_remove(request, product_id):
+    """ Представление удаления товаров из корзины."""
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product=product)
+    return redirect('cart:cart_detail')
+
+
+def cart_detail(request):
+    """Представление отображения корзины и ее товаров."""
+    cart = Cart(request)
+    return render(request, 'cart/detail.html', {'cart': cart})
