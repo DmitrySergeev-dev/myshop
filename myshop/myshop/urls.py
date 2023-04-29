1195ef16787746fcbcbc31034c6f45a00802b364
Dmitry Sergeev <dimonsergeev10@mail.ru>
2023-04-29 23:34:08 +0300

    Finish order creating functional

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -1,11 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('', include('shop.urls', namespace='shop')),
 
 ]
